b5f9e372f6fb76ae2b3ced7b6718f47a32b91fd2
Jefferson Stachelski <jefferson.stachelski@gmail.com>
2018-12-19 16:23:27 -0200

    Adapt chanllenge Day1 with the new input format

--- lib/advent_of_code_2018/day1.ex
+++ lib/advent_of_code_2018/day1.ex
@@ -1,13 +1,20 @@
 defmodule AdventOfCode2018.Day1 do
-  def calc_frequency_pt1(command) do
-    command
-    |> parse_command()
+  def calc_frequency_pt1(raw_values) do
+    raw_values
+    |> parse_to_integer()
     |> Enum.reduce(0, &(&1 + &2))
   end
 
-  def calc_frequency_pt2(command) do
-    values = parse_command(command)
+  def calc_frequency_pt2(raw_values) do
+    values = parse_to_integer(raw_values)
     next_frequency(values, values, 0, [])
+  end
+
+  defp parse_to_integer(raw_values) do
+    Enum.map(raw_values, fn raw_value ->
+      {num, ""} = Integer.parse(raw_value)
+      num
+    end)
   end
 
   defp next_frequency(original_values, [], current, old_results) do
@@ -20,12 +27,6 @@ defmodule AdventOfCode2018.Day1 do
     else
       next_frequency(original_values, rest, current + next, [current | old_results])
     end
-  end
-
-  defp parse_command(command) do
-    command
-    |> String.split(",")
-    |> Enum.map(&parse_str_to_int/1)
   end
 
   defp parse_str_to_int(str) do
