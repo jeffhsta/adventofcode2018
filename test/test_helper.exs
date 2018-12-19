860c2b06ba19628fe50594688e269c8bc077bcd0
Jefferson Stachelski <jefferson.stachelski@gmail.com>
2018-12-19 16:22:56 -0200

    Move challenge inputs to a separated folder for testing

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,12 @@
 ExUnit.start()
+
+defmodule TestHelper do
+  @input_files_path "test/challenge_input/"
+
+  def load_input_from_file(challenge) do
+    "#{@input_files_path}#{challenge}.txt"
+    |> File.read!()
+    |> String.trim()
+    |> String.split("\n")
+  end
+end
