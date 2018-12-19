d4afab6f2b13eb7099a4addcd2a821d67c5a3385
Jefferson Stachelski <jefferson.stachelski@gmail.com>
2018-12-19 16:43:53 -0200

    Fix Day1 and Day2, make more clear the assertion

--- test/advent_of_code_2018/day1_test.exs
+++ test/advent_of_code_2018/day1_test.exs
@@ -5,27 +5,23 @@ defmodule AdventOfCode2018.Day1Test do
 
   describe "Day 1 Part 1" do
     test "return the expected answer from the challenge input" do
-      expected = 525
-
       result =
         :day1
         |> load_input_from_file()
         |> Day1.calc_frequency_pt1()
 
-      assert expected == result
+      assert result == 525
     end
   end
 
   describe "Day 1 Part 2" do
     test "return the expected answer from the challenge input" do
-      expected = 75749
-
       result =
         :day1
         |> load_input_from_file()
         |> Day1.calc_frequency_pt2()
 
-      assert expected == result
+      assert result == 75_749
     end
   end
 end
--- test/advent_of_code_2018/day2_test.exs
+++ test/advent_of_code_2018/day2_test.exs
@@ -1,33 +1,27 @@
 defmodule AdventOfCode2018.Day2Test do
   use ExUnit.Case
-
   import TestHelper
-
   alias AdventOfCode2018.Day2
 
   describe "Day 2 Part 1" do
     test "return the expected answer from the challenge input" do
-      expected = 7221
-
       result =
         :day2
         |> load_input_from_file()
         |> Day2.checksum()
 
-      assert expected == result
+      assert result == 7_221
     end
   end
 
   describe "Day 2 Part 2" do
     test "return the expected answer from the challenge input" do
-      expected = "mkcdflathzwsvjxrevymbdpoq"
-
       result =
         :day2
         |> load_input_from_file()
         |> Day2.find_similar_box_id()
 
-      assert expected == result
+      assert result == "mkcdflathzwsvjxrevymbdpoq"
     end
   end
 end
